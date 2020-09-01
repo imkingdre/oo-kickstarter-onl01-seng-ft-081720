a86172a22d3bd93a14e9ef79b29abcb9b67ab6a2
Andre King <andreking1031@gmail.com>
2020-09-01 17:55:29 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,8 +1,8 @@
 class Backer
   attr_accessor :name, :backed_projects
-  @@name = []
+  
   def initialize(name)
-    @@name = name
+    @name = name
     @backed_projects = []
   end
   def backed_projects(project)
